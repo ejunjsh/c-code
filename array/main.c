6d6968cdafcc004da00f8435f3e9994a56fb69e2
zhouff <sjj050121014@163.com>
2018-07-12 13:24:16 +0800

    minor changes

--- array/main.c
+++ array/main.c
@@ -5,17 +5,17 @@ void changeA(int *a){
 }
 
 void changeB(int b[]){
-    b[0]=10;
+    b[1]=20;
 }
 
 int main(){
     int a[5]={1,2,3,4,5};
     changeA(a);
-    printf("%d\n",a[0]);
+    printf("%d %d %d %d %d \n",a[0],a[1],a[2],a[3],a[4]);
 
-    int b[5]={1,2,3,4,5};
+    int b[5]={6,7,8,9,10};
     changeB(b);
-    printf("%d\n",b[0]);
+    printf("%d %d %d %d %d \n",b[0],b[1],b[2],b[3],b[4]);
 
     int *c={1,2,3,4,5};
     changeA(c);
